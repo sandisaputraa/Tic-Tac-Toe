3a32adef224219816afe768a9a0f251cc5b023f8
sandisaputraa <104289275+sandisaputraa@users.noreply.github.com>
2024-07-23 01:29:17 +0700

    Create tic-tac-toe.py
    scract awal

---
+++ tic-tac-toe.py
@@ -0,0 +1,22 @@
+def display_board(board):
+    # The function accepts one parameter containing the board's current status
+    # and prints it out to the console.
+
+
+def enter_move(board):
+    # The function accepts the board's current status, asks the user about their move, 
+    # checks the input, and updates the board according to the user's decision.
+
+
+def make_list_of_free_fields(board):
+    # The function browses the board and builds a list of all the free squares; 
+    # the list consists of tuples, while each tuple is a pair of row and column numbers.
+
+
+def victory_for(board, sign):
+    # The function analyzes the board's status in order to check if 
+    # the player using 'O's or 'X's has won the game
+
+
+def draw_move(board):
+    # The function draws the computer's move and updates the board.
